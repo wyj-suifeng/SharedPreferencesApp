bf15af6e133d1917cc91e2003eca436dc51aa652
wyj-suifeng <1041457754@qq.com>
2016-09-06 10:46:14 +0800

    Initial commit

---
+++ app/src/main/java/com/example/lenovo/sharedpreferencesapp/MainActivity.java
@@ -0,0 +1,96 @@
+package com.example.lenovo.sharedpreferencesapp;
+
+import android.annotation.TargetApi;
+import android.content.SharedPreferences;
+import android.icu.text.SimpleDateFormat;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.sql.Date;
+
+public class MainActivity extends AppCompatActivity {
+    private final static String SharedPreferencesFileName="config";
+
+    //键
+    private final static String Key_UserName="UserName";//用户名
+    private final static String Key_LoginDate="LoginDate";//登录时间
+    private final static String Key_UserType="UserType";
+
+    SharedPreferences preferences;
+    SharedPreferences.Editor editor;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //获得SharedPreferences实例
+        preferences=getSharedPreferences(SharedPreferencesFileName, MODE_PRIVATE);
+        editor=preferences.edit();
+
+        Button btnWrite=(Button)findViewById(R.id.ButtonWrite);
+        Button btnRead=(Button)findViewById(R.id.ButtonRead);
+        btnWrite.setOnClickListener(new View.OnClickListener() {
+            @TargetApi(Build.VERSION_CODES.N)
+            @Override
+            public void onClick(View view) {
+
+                //格式化日期，将日期按照年月日时分秒格式转换为字符串形式
+                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+                String strLoginDate = simpleDateFormat.format(new java.util.Date());
+
+                //写入键值对
+                editor.putString(Key_UserName, "王玉杰");
+                editor.putString(Key_LoginDate, strLoginDate);
+                editor.putInt(Key_UserType, 2014011213);
+
+                //提交修改，此处换成commit()也可以
+                editor.apply();
+            }
+        });
+
+        btnRead.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String strUserName = preferences.getString(Key_UserName, null);
+                String strLoginDate = preferences.getString(Key_LoginDate, null);
+                int nUserType=preferences.getInt(Key_UserType,0);
+                if (strUserName != null && strLoginDate != null)
+                    Toast.makeText(MainActivity.this, strUserName + "登录时间:" +"用户名:" +  strLoginDate+"学号:"+nUserType, Toast.LENGTH_LONG).show();
+                else
+                    Toast.makeText(MainActivity.this, "无数据", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
